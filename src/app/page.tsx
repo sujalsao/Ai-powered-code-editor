61eb3ef35c903f5c0d8490c97fe479c50581cc06
Sujal Sao <sujalsao67@gmail.com>
2026-06-15 15:03:25 +0530

    02-database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <Button>Get Started</Button>
